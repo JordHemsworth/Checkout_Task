49db3b846b23651b852b6810ebe5277847b1b526
jordhemsworth <88255447+JordHemsworth@users.noreply.github.com>
2023-08-27 11:44:25 +0100

    Step 2

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -5,13 +5,20 @@ object Checkout {
   def calculateTotalCost(items: List[String]): BigDecimal = {
     val appleCount = items.count(_ == "Apple")
     val orangeCount = items.count(_ == "Orange")
-    val totalCost = appleCount * applePrice + orangeCount * orangePrice
+
+    val appleDiscountedCount = appleCount / 2
+    val appleTotal = appleCount - appleDiscountedCount
+
+    val orangeTotal = ((orangeCount / 3) * 2) + (orangeCount % 3)
+
+    val totalCost = appleTotal * applePrice + orangeTotal * orangePrice
     totalCost
   }
 
   def main(args: Array[String]): Unit = {
-    val scannedItems = List("Apple", "Apple", "Orange", "Apple")
+    val scannedItems = List("Apple", "Apple", "Orange", "Apple", "Orange","Orange")
     val totalCost = calculateTotalCost(scannedItems)
     println(f"Total cost: £$totalCost%.2f")
   }
+
 }
--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -4,9 +4,9 @@ import org.scalatest.matchers.should.Matchers
 class CheckoutSpec extends AnyFlatSpec with Matchers {
 
   "Checkout" should "calculate total cost correctly for a list of items" in {
-    val items = List("Apple", "Apple", "Orange", "Apple")
+    val items = List("Apple", "Apple", "Apple", "Orange", "Orange", "Orange" )
     val totalCost = Checkout.calculateTotalCost(items)
-    totalCost shouldEqual 2.05
+    totalCost shouldEqual 1.7 // Price of 2 apples, 2 oranges
   }
 
   it should "calculate total cost as 0 for an empty list" in {
@@ -21,15 +21,27 @@ class CheckoutSpec extends AnyFlatSpec with Matchers {
     totalCost shouldEqual 0.0
   }
 
-  it should "calculate total cost correctly for a list with only apples" in {
+  it should "calculate total cost correctly for a list with only apples with offer" in {
     val items = List("Apple", "Apple", "Apple", "Apple")
     val totalCost = Checkout.calculateTotalCost(items)
-    totalCost shouldEqual 2.4
+    totalCost shouldEqual 1.2 //Price of 2
   }
 
-  it should "calculate total cost correctly for a list with only oranges" in {
+  it should "calculate total cost correctly for a list with only oranges without the offer" in {
     val items = List("Orange", "Orange")
     val totalCost = Checkout.calculateTotalCost(items)
     totalCost shouldEqual 0.5
+  }
+
+  it should "calculate total cost correctly for a list with only oranges with the offer" in {
+    val items = List("Orange", "Orange", "Orange")
+    val totalCost = Checkout.calculateTotalCost(items)
+    totalCost shouldEqual 0.5 //Price of 2
+  }
+
+  it should "calculate total cost correctly for a list with more than three oranges with the offer" in {
+    val items = List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+    val totalCost = Checkout.calculateTotalCost(items)
+    totalCost shouldEqual 1.0 //Price of 4
   }
 }
